fa84fc2bc6bbd175ca065582a171576d5be1bcd4
ferry <ferryaze1@gmail.com>
2018-08-01 10:10:14 +0900

    meta

--- resources/views/frontend/layout/master.blade.php
+++ resources/views/frontend/layout/master.blade.php
@@ -7,6 +7,28 @@
 		<meta name="description" content="The best K-Pop, Korean Clothes, Korean Fashion, Korean Trends Distributor and Forwarder, contact us now!, Korean Clothes, Korean Shoes, Korean Cosmetic, Korean Product, Korean Fashion, Korean Style, Korean Language, K-Pop Clothes, K-Pop Shoes, K-Pop Cosmetic, K-Pop Product, K-Pop Fashion, K-Pop Style, Baju Korea, Sepatu Korea, Produk Korea, Kosmetik Korea, Fashion Korea, Bahasa Korea, Semua tentang Korea, Export Korea, Import Korea, Forwarder Korea, Kios, Korea, KiosKorea, www.kioskorea.com, kioskorea.com">
     	<meta name="keywords" content="The best K-Pop, Korean Clothes, Korean Fashion, Korean Trends Distributor and Forwarder, contact us now!, Korean Clothes, Korean Shoes, Korean Cosmetic, Korean Product, Korean Fashion, Korean Style, Korean Language, K-Pop Clothes, K-Pop Shoes, K-Pop Cosmetic, K-Pop Product, K-Pop Fashion, K-Pop Style, Baju Korea, Sepatu Korea, Produk Korea, Kosmetik Korea, Fashion Korea, Bahasa Korea, Semua tentang Korea, Export Korea, Import Korea, Forwarder Korea, Kios, Korea, KiosKorea, www.kioskorea.com, kioskorea.com">
     	<meta name="author" content="www.kioskorea.com">
+
+
+		<!--  Essential META Tags -->
+
+		<meta property="og:title" content="KiosKorea The best K-Pop, Korean Clothes, Korean Fashion, Korean Trends Distributor and Forwarder, contact us now!">
+		<meta property="og:description" content="Korean Clothes, Korean Shoes, Korean Cosmetic, Korean Product, Korean Fashion, Korean Style, Korean Language, K-Pop Clothes, K-Pop Shoes, K-Pop Cosmetic, K-Pop Product, K-Pop Fashion, K-Pop Style, Baju Korea, Sepatu Korea, Produk Korea, Kosmetik Korea, Fashion Korea, Bahasa Korea, Semua tentang Korea, Export Korea, Import Korea, Forwarder Korea, Kios, Korea, KiosKorea, www.kioskorea.com, kioskorea.com">
+		<meta property="og:image" content="{{ asset('frontend/images/pic2.png') }}">
+		<meta property="og:url" content="https://www.kioskorea.com">
+		<meta name="twitter:card" content="KiosKorea The best K-Pop, Korean Clothes, Korean Fashion, Korean Trends Distributor and Forwarder, contact us now! Korean Clothes, Korean Shoes, Korean Cosmetic, Korean Product, Korean Fashion, Korean Style, Korean Language, K-Pop Clothes, K-Pop Shoes, K-Pop Cosmetic, K-Pop Product, K-Pop Fashion, K-Pop Style, Baju Korea, Sepatu Korea, Produk Korea, Kosmetik Korea, Fashion Korea, Bahasa Korea, Semua tentang Korea, Export Korea, Import Korea, Forwarder Korea, Kios, Korea, KiosKorea, www.kioskorea.com, kioskorea.com">
+
+
+		<!--  Non-Essential, But Recommended -->
+
+		<meta property="og:site_name" content="KiosKorea The best K-Pop, Korean Clothes, Korean Fashion, Korean Trends Distributor and Forwarder, contact us now!">
+		<meta name="twitter:image:alt" content="Korean Clothes, Korean Shoes, Korean Cosmetic, Korean Product, Korean Fashion, Korean Style, Korean Language, K-Pop Clothes, K-Pop Shoes, K-Pop Cosmetic, K-Pop Product, K-Pop Fashion, K-Pop Style, Baju Korea, Sepatu Korea, Produk Korea, Kosmetik Korea, Fashion Korea, Bahasa Korea, Semua tentang Korea, Export Korea, Import Korea, Forwarder Korea, Kios, Korea, KiosKorea, www.kioskorea.com, kioskorea.com">
+
+
+		<!--  Non-Essential, But Required for Analytics -->
+
+		{{-- <meta property="fb:app_id" content="your_app_id" />
+		<meta name="twitter:site" content="@website-username"> --}}
+
 		<meta name="csrf-token" content="{{ csrf_token() }}">
 		<link rel="icon" href="{{ asset('frontend/images/icon.png') }}">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.1.0/css/all.css" integrity="sha384-lKuwvrZot6UHsBSfcMvOkWwlCMgc0TaWr+30HWe3a4ltaBwTZhyTEggF5tJv8tbt" crossorigin="anonymous">
